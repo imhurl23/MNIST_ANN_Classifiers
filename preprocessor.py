72e5102facdd5a33f94beba7e983cea1ba86a710
imhurl23 <imhurl23@colby.edu>
2022-05-02 20:00:36 +0200

    migrating preproccing information into base python and out of development notebook

---
+++ preprocessor.py
@@ -0,0 +1,12 @@
+%matplotlib inline
+
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import sklearn
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+from torch.utils.data import Dataset, DataLoader
